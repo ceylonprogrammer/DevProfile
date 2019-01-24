923b98e41055301662c32769a5c573437244bce2
Sameera Dissanayaka <sameerad177@gmail.com>
2019-01-25 00:42:58 +0530

    Added xlarge device layout

--- app/src/main/java/com/example/sameera/devprofile/MainActivity.kt
+++ app/src/main/java/com/example/sameera/devprofile/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.sameera.devprofile
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //making logo image with round coners
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+        logo.setImageDrawable(rounded)
+        //else you can use rounded image
+//        rounded.isCircular=true
+//        logo.setImageDrawable(rounded)
     }
 }
